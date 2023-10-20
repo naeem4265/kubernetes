df70f3b190f6784994d58f11df67c712c075ac40
MD.NAEEM-AL-IMRAN <md.naeem.al.imran@gmail.com>
2023-10-20 16:03:40 +0600

    Delete go directory

--- go/D04/VariableDeclaration.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var s string = "Naeem AL Imran"
-	pos := "Software Engineer"
-	fmt.Println(s + " \n" + pos)
-}
--- go/D04/stringIndexAccess.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func main(){
-	fmt.Println("Naeem"[1])
-}
